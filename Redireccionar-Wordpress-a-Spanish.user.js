db451255a31537b0454cb99df969663919e96e61
Chris Silupu <cesarchris710@gmail.com>
2024-04-11 17:28:28 -0500

    Update Redireccionar-Wordpress-a-Spanish.user.js
    Se añadió /themes/ para español

--- Redireccionar-Wordpress-a-Spanish.user.js
+++ Redireccionar-Wordpress-a-Spanish.user.js
@@ -4,12 +4,13 @@
 // @homepageURL  https://github.com/CesarChris710/redireccionar-wordpress-a-spanish/
 // @downloadURL  https://raw.githubusercontent.com/CesarChris710/redireccionar-wordpress-a-spanish/master/Redireccionar-Wordpress-a-Spanish.user.js
 // @updateURL    https://raw.githubusercontent.com/CesarChris710/redireccionar-wordpress-a-spanish/master/Redireccionar-Wordpress-a-Spanish.user.js
-// @version      1.0
+// @version      1.1
 // @description  Redireccionar URLs de WordPress.org a su versión en español
 // @author       CesarChris710
 // @match        *://*.wordpress.org/plugins/*
+// @match        *://*.wordpress.org/themes/*
 // @exclude      *://es.wordpress.org/plugins/*
-
+// @exclude      *://es.wordpress.org/themes/*
 // @grant        none
 // ==/UserScript==
 
@@ -17,7 +18,12 @@
     'use strict';
     function redireccionarURL() {
         var urlActual = window.location.href;
-        var nuevaURL = urlActual.replace(/\/\/[^/]+\/plugins\//, "//es.wordpress.org/plugins/");
+        var nuevaURL;
+        if (urlActual.includes("/plugins/")) {
+            nuevaURL = urlActual.replace(/\/\/[^/]+\/plugins\//, "//es.wordpress.org/plugins/");
+        } else if (urlActual.includes("/themes/")) {
+            nuevaURL = urlActual.replace(/\/\/[^/]+\/themes\//, "//es.wordpress.org/themes/");
+        }
         window.location.replace(nuevaURL);
     }
     redireccionarURL();
